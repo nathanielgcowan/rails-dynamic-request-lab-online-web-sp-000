3095cd42e84eb2ccaaed09df8e299ee44229cb8b
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-02-02 21:10:32 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,12 +2,9 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
-<<<<<<< HEAD
 
   def show
     @student = Student.find_by(params[:id])
   end
 end
-=======
 end
->>>>>>> 3ac9d08c932478a1aada1ff4c61ae26d7e7a2461
